151f6cd20ecef0897b7cd2dccc02552cdcdc21df
Michael Slowik <slowik.michael@gmail.com>
2020-01-13 01:06:37 -0300

    Extra listing id validation

--- src/StorageBuilder.php
+++ src/StorageBuilder.php
@@ -106,7 +106,13 @@ class StorageBuilder
      */
     public function getListingItem($id)
     {
-        return $this->endpoint('listings/'.$this->getListing().'/items')
+        $listingId = $this->getListing();
+
+        if (empty($listingId)) {
+            throw new \Exception('Please set listing id.');
+        }
+
+        return $this->endpoint('listings/'.$listingId.'/items')
             ->find($id);
     }
 
